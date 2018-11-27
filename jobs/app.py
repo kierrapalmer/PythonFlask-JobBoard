3a838c7fb5116635ec8c0fae242add0dfb4f915f
Kierra Palmer <kierrapalmer@weber.edu>
2018-11-27 12:10:42 -0700

    fix  module 8

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3
 import datetime
 
@@ -55,16 +55,18 @@ def employer(employer_id):
     return render_template("employer.html", employer= employer, jobs=jobs, reviews=reviews)
 
 
-
 @app.route("/employer/<employer_id>/review", methods=('GET', 'POST'))
 def review(employer_id):
-    review = request.form['review']
-    rating = request.form['rating']
-    title = request.form['title']
-    status = request.form['status']
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
 
-    date = datetime.datetime.now().strftime("%m/%d/%Y")
-    execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
+
     return render_template('review.html', employer_id=employer_id)
 
 
